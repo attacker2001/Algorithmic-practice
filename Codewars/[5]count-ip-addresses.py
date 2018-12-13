cdcc15f48b4b4ab22fe535f90cb9895c365c9ba6
attacker2001 <attacker20012@gmail.com>
2018-12-13 18:02:38 +0800

    daily practices.

--- Codewars/[5]count-ip-addresses.py
+++ Codewars/[5]count-ip-addresses.py
@@ -11,9 +11,11 @@
 """
 https://www.codewars.com/kata/count-ip-addresses/train/python
 
-Write a function that accepts a starting and ending IPv4 address, and the number of IP addresses from start to end, excluding the end IP address.
+Write a function that accepts a starting and ending IPv4 address, 
+and the number of IP addresses from start to end, excluding the end IP address.
 
-All input to the function will be valid IPv4 addresses in the form of strings. The ending address will be at least one address higher than the starting address.
+All input to the function will be valid IPv4 addresses in the form of strings. 
+The ending address will be at least one address higher than the starting address.
 
 Examples
 ipsBetween("10.0.0.0", "10.0.0.50")  =>   50 
