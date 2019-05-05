f09af254bfd0da41ae35594a813a3dc6d10e9597
attacker2001 <attacker20012@gmail.com>
2019-05-05 16:12:49 +0800

    added self understanding.

--- leetcode/204. Count Primes.py
+++ leetcode/204. Count Primes.py
@@ -114,7 +114,7 @@ class Solution(object):
                 #          //i        # get number of groups
                 #              + 1    # get number of slots
                 # strikes[2*2:11:2]  = [0] * ((11-1-2*2)//2 + 1
-                # strikes[4:11:2]    = [0] * 4
+                # strikes[4:11:2]    = [0] * 4              # !!!!!! Here is a candy of Python !!!!!!!!
                 # s[4], s[6], s[8], s10] = 0, 0, 0, 0
                 strikes[i*i:n:i] = [0] * ((n-1-i*i)//i + 1)
 
